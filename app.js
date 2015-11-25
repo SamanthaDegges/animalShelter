f97e7cd912c68fdf5879eeb598f8bae6cf8a812e
Samantha Degges <samanthadegges@gmail.com>
2015-11-24 19:43:10 -0800

    edited middleware for clients routes.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', routes);
+app.use('/clients', client);
 app.use('/animals', animals);
 
 app.get('/', function(req, res) {
