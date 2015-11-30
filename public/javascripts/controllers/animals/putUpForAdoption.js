dcd30c5ee30e088174e307784100ee5b276c26be
Samantha Degges <samanthadegges@gmail.com>
2015-11-30 09:59:43 -0800

    added alert on error

--- public/javascripts/controllers/animals/putUpForAdoption.js
+++ public/javascripts/controllers/animals/putUpForAdoption.js
@@ -1,6 +1,6 @@
 var routerApp = angular.module('routerApp');
 
-routerApp.service('AddPet', function($http) {
+routerApp.service('addPet', function($http) {
   this.createAndAddPet = function(animal) {
     var newAnimal = {
       kind: animal.kind,
@@ -13,14 +13,15 @@ routerApp.service('AddPet', function($http) {
     $http.post("/animals", newAnimal).
     then(function(err){
       console.log(err);
+      alert("There's been an error saving this addition to the database. Please try again.")
     }, function(success){
       console.log(success);
     })
   }
 });
 
-routerApp.controller('putUpForAdoptionCtrl', function($scope, $http, AddPet){
+routerApp.controller('putUpForAdoptionCtrl', function($scope, $http, addPet){
   $scope.submitToPost = function() {
-    AddPet.createAndAddPet($scope.animal);
+    addPet.createAndAddPet($scope.animal);
   }
 });
